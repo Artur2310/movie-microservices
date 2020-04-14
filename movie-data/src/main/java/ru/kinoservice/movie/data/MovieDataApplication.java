ac2b6b4c77ecac5a406cb4d813f6d923caedddc1
Artur <artur2310@mail.ru>
2020-04-14 19:14:56 +0300

    Create movie-data

---
+++ movie-data/src/main/java/ru/kinoservice/movie/data/MovieDataApplication.java
@@ -0,0 +1,13 @@
+package ru.kinoservice.movie.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDataApplication.class, args);
+	}
+
+}
---
+++ movie-data/src/test/java/ru/kinoservice/movie/data/MovieDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kinoservice.movie.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
