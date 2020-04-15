47d6c924e9d064c4ecbae070d0be62fb84e4baae
Artur <artur2310@mail.ru>
2020-04-15 19:03:47 +0300

    Create general-parser

---
+++ general-parser/src/main/java/ru/kinoservice/general/parser/GeneralParserApplication.java
@@ -0,0 +1,13 @@
+package ru.kinoservice.general.parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralParserApplication.class, args);
+	}
+
+}
---
+++ general-parser/src/test/java/ru/kinoservice/general/parser/GeneralParserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kinoservice.general.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
